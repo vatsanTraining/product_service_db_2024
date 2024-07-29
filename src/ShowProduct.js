fb4967022b2fd98dbb8221e2ef5a3bdb48507ad7
srivatsan <vatsan_k@hotmail.com>
2024-07-29 15:55:16 +0530

    Making Rest API Calls from React Component

--- src/ShowProduct.js
+++ src/ShowProduct.js
@@ -1,9 +1,36 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useData } from "./useData";
+import axios from "axios";
 
 const ShowProduct = () => {
-  const { data } = useData("http://localhost:5550/api/v1/products");
-  return <div>{data}</div>;
+  //   const { data } = useData("http://localhost:5550/api/v1/products");
+
+  const [data, setData] = useState([]);
+
+  const handleData = (eachElement, idx) => {
+    return(
+      <div key={idx}>
+        <p>{eachElement.productName}</p>
+        <p>{eachElement.ratePerUnit}</p>
+      </div>
+    );
+  };
+
+  const fetchData = async () => {
+    const response = await axios.get("http://localhost:5550/api/v1/products");
+
+    setData(response.data);
+  };
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      <h1>Product List</h1>
+      {data.map(handleData)}
+    </div>
+  );
 };
 
 export default ShowProduct;
--- src/useData.js
+++ src/useData.js
@@ -1,17 +1,14 @@
-import { useState,useEffect } from "react";
+import { useState, useEffect } from "react";
 import axios from "axios";
 export const useData = (url) => {
+  const [data, setData] = useState([{}]);
 
-    const [data,setData]=useState([])
-
- const fetchData = async() =>{
-
- const response =await axios.get(url);
-  setData(response.data);
- }
-  useEffect(() =>{
-  fetchData();
-},[]);
- return {data};
-  
-}
+  const fetchData = async () => {
+    const response = await axios.get(url);
+    setData(response.data);
+  };
+  useEffect(() => {
+    fetchData();
+  }, []);
+  return { data };
+};
